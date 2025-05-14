0e231fff0cff53e9a9cb5523d3c4ccf83a378bd3
nscaperdas <nicholasscaperdas@gmail.com>
2025-05-13 23:13:17 -0400

    added project js functionality
    projects open and close

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
+//hamburger menu toggle
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+//Projects Accordion
+document.addEventListener("DOMContentLoaded", () => {
+  //Selects whole project section after title
+  const projectContainer = document.querySelector(".project-content");
+
+  //finds the nearest clicked project
+  projectContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".project-group-header");
+
+    if (!groupHeader) {
+      console.log("fail");
+      console.log(groupHeader);
+      console.log(projectContainer);
+      return;
+    }
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector(".project-group-body");
+    const icon = groupHeader.querySelector("i");
+
+    //Toggle Icon
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    //Toggle visibility of body
+    groupBody.classList.toggle("open");
+
+    //Close other open FAQ bodies
+    const otherGroups = projectContainer.querySelectorAll(".project-group");
+    console.log(otherGroups);
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".project-group-body");
+        const otherIcon = otherGroup.querySelector(".project-group-header i");
+
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
+  });
+});
